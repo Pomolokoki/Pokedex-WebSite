631dbd0315ae8c14a5cb1d6ed1d3b64e6f097dbd
Chanfi Mari Yram <152385957+JusDeFruits1@users.noreply.github.com>
2024-09-20 16:11:57 +0200

    Avancement sur le header
    Ajout des liens vers les différentes pages pour la navigation + amélioration des boutons

--- js/header.js
+++ js/header.js
@@ -30,6 +30,10 @@ document.getElementById('pokedexPage').addEventListener('click',function(){
   document.location.href = './Pokedex/Pokedex.php';
 });
 
+document.getElementById('typeTablePage').addEventListener('click',function(){
+  document.location.href = 'typeTable.php';
+});
+
 document.getElementById('mapPage').addEventListener('click',function(){
-  document.location.href = 'map.html';
+  document.location.href = 'map.php';
 });
