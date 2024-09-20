6500a8932725c4179387984137f09e345c56603c
Chanfi Mari Yram <152385957+JusDeFruits1@users.noreply.github.com>
2024-09-20 15:30:09 +0200

    Avancement sur le header
    Bouton de navigation ajoutés, embellissement des boutons s'inscrire et connexion.

--- js/header.js
+++ js/header.js
@@ -1,28 +1,35 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var button = document.createElement('button');
-    button.type = 'button';
-    button.innerHTML = "Connexion";
-    button.className = 'Connexion';
-    button.id = "Login";
-    // button.style.backgroundColor = "yellow";
-  
-    button.onclick = function() {
-      // ...
-    };
+document.addEventListener('DOMContentLoaded', function () {
+  var login = document.createElement('button');
+  var register = document.createElement('button');
 
-    var button2 = document.createElement('button');
-    button2.type = 'button';
-    button2.innerHTML = "S'inscrire";
-    button2.className = 'Connexion';
-    button2.id = "Register"
-    // button2.style.backgroundColor = "green";
-  
-    button2.onclick = function() {
-      // ...
-    };
+  /*Paramètres du bouton login*/
+  login.type = 'button';
+  login.innerHTML = "Connexion";
+  login.className = 'Connexion';
+  login.id = "Login";
+  login.onclick = function () {
+    // ...
+  };
+  /*Paramètres du bouton register*/
+  register.type = 'button';
+  register.innerHTML = "S'inscrire";
+  register.className = 'Connexion';
+  register.id = "Register"
+  register.onclick = function () {
+    // ...
+  };
 
-    var container = document.getElementById('Access');
-    container.appendChild(button);
-    container.appendChild(button2);
-    
-  }, false);
+
+  var container = document.getElementById('Profile');
+  container.appendChild(login);
+  container.appendChild(register);
+
+}, false);
+
+document.getElementById('pokedexPage').addEventListener('click',function(){
+  document.location.href = './Pokedex/Pokedex.php';
+});
+
+document.getElementById('mapPage').addEventListener('click',function(){
+  document.location.href = 'map.html';
+});
