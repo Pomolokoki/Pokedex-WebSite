a58efe552396f2d7d7931eb8f7da327e82310078
Eloups <121400663+Eloups@users.noreply.github.com>
2025-05-13 11:32:44 +0200

    fix - create DB at first server start

--- src/database/connection/create/loadDataIntoWebsite.php
+++ src/database/connection/create/loadDataIntoWebsite.php
@@ -1,34 +1,30 @@
 <?php
 function loadSQL($file)
 {
-    if (file_exists($file))
-    {
+    if (file_exists($file)) {
         $sql = explode('INSERT INTO', file_get_contents($file));
         global $db;
-        for ($i = 0; $i < count($sql); $i++)
-        {
-            echo ''. $sql[$i] .'<br><br>';
-            if ($i == 0)
-            {
+        for ($i = 0; $i < count($sql); $i++) {
+            if ($i == 0) {
                 // $createQuerys = explode('DELIMITER', $sql[$i]);
                 // for ( $j = 0; $j < count($createQuerys); $j++ )
                 // {
-                    // echo ' ' . $createQuerys[$j] . '<br>' . $j . '<br>';
-                    // if ($j%2 == 1)
-                    // {
-                        // echo ' DELIMITER | <br><br>';
-                        // $statemnt = $db->prepare('DELIMITER |');
-                        // $statemnt->execute();
-                    // }
-                    // else if ($j > 0)
-                    // {
-                        // echo ' DELIMITER ; <br><br>';
-                        // $statemnt = $db->prepare('DELIMITER ;');
-                        // $statemnt->execute();
-                    // }
-                    // $statemnt = $db->prepare($createQuerys[$j]);
-                    $statemnt = $db->prepare($sql[$i]);
-                    $statemnt->execute();
+                // echo ' ' . $createQuerys[$j] . '<br>' . $j . '<br>';
+                // if ($j%2 == 1)
+                // {
+                // echo ' DELIMITER | <br><br>';
+                // $statemnt = $db->prepare('DELIMITER |');
+                // $statemnt->execute();
+                // }
+                // else if ($j > 0)
+                // {
+                // echo ' DELIMITER ; <br><br>';
+                // $statemnt = $db->prepare('DELIMITER ;');
+                // $statemnt->execute();
+                // }
+                // $statemnt = $db->prepare($createQuerys[$j]);
+                $statemnt = $db->prepare($sql[$i]);
+                $statemnt->execute();
                 // }
                 continue;
             }
@@ -40,7 +36,6 @@ function loadSQL($file)
 
 function loadDB()
 {
-    loadSQL('../../create/pokedexFromPhp.sql');
-    loadSQL('../create/pokedexFromPhp.sql');
+    loadSQL(__DIR__ . '/../../create/pokedexFromPhp.sql');
 }
 loadDB();
